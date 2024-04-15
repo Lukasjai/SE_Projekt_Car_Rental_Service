7ec475524f7c994e0f1f269a5a94327ac7335e5b
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-04-15 19:01:04 +0200

    Replace sql logic with customerdto

--- src/main/java/org/example/controller/CustomerController.java
+++ src/main/java/org/example/controller/CustomerController.java
@@ -1,15 +1,15 @@
 package org.example.controller;
 
 import jakarta.servlet.http.HttpSession;
-import org.example.Customer;
-import org.example.CustomerRepository;
+import org.example.model.Customer;
+import org.example.repository.CustomerRepository;
 import org.example.database.MariaDB;
+import org.example.dto.CustomerDto;
+import org.example.service.CustomerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.util.Optional;
 
 @RestController
@@ -17,55 +17,21 @@ import java.util.Optional;
 public class CustomerController {
 
     private final CustomerRepository customerRepository;
-    private MariaDB databaseConnection = new MariaDB();
-    private String customer_firstname;
-    private String customer_lastname;
-    private String customer_telephone_number = "06601230879";
-    private String email;
-    private String password;
-    private int licence_number = 123456;
-    private String insertQuery = "INSERT INTO customers (customer_firstname, customer_lastname, customer_telephone_number, licence_number, password, email) VALUES (?, ?, ?, ?, ?,?)";
+    private final CustomerService customerService;
 
-    public CustomerController(CustomerRepository customerRepository) {
+    public CustomerController(CustomerRepository customerRepository, CustomerService customerService) {
         this.customerRepository = customerRepository;
+        this.customerService = customerService;
     }
-  
-
 
     @CrossOrigin
     @PostMapping("/register")
-    public ResponseEntity<?> registerCustomer(@RequestParam String vorname, @RequestParam String nachname, @RequestParam String email, @RequestParam String password) {
+    public ResponseEntity<?> registerCustomer(@RequestBody CustomerDto customerDto) {
         try {
-            Customer customer = new Customer();
-            customer.setfirstName(vorname);
-            customer.setlastName(nachname);
-            customer.setEmail(email);
-            customer.setPassword(password);
-            this.customer_firstname = vorname;
-            this.customer_lastname = nachname;
-            this.email = email;
-            this.customer_telephone_number = password;
-            Connection connection = databaseConnection.getConnection();
-            insertQuery = "INSERT INTO customers (customer_firstname, customer_lastname, customer_telephone_number, licence_number, password, email) VALUES (?, ?, ?, ?, ?,?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
 
-            //ToDo: Validation Checks
-            preparedStatement.setString(1, this.customer_firstname);
-            preparedStatement.setString(2, this.customer_lastname);
-            preparedStatement.setString(3, this.customer_telephone_number);
-            preparedStatement.setInt(4, this.licence_number);
-            preparedStatement.setString(5, password);
-            preparedStatement.setString(6, email);
-            int rowsAffected = preparedStatement.executeUpdate();
-            System.out.println(rowsAffected + " row(s) inserted.");
+            Customer customer = customerService.registerCustomer(customerDto);
 
-            // Close resources
-            preparedStatement.close();
-            connection.close();
-
-            Customer savedCustomer = customerRepository.save(customer);
-
-            return ResponseEntity.status(HttpStatus.CREATED).body(savedCustomer);
+            return ResponseEntity.status(HttpStatus.CREATED).body(customer);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to register customer");
         }
@@ -106,7 +72,7 @@ public class CustomerController {
         }
     }
 
-    @PatchMapping("/update/{customerId}")
+    /*@PatchMapping("/update/{customerId}")
     public ResponseEntity<?> updateCustomer(@PathVariable Long customerId, @RequestBody Customer updatedCustomer) {
         try {
             Customer existingCustomer = customerRepository.findById(customerId).orElse(null);
@@ -132,5 +98,5 @@ public class CustomerController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to update customer");
         }
-    }
+    }*/
 }
