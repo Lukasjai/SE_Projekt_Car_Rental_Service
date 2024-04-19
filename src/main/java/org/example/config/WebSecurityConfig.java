18723f0fb85ae5eab152c77f20f0f3cb089ad431
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-04-19 16:29:31 +0200

    Add basic WebSecurityConfig

---
+++ src/main/java/org/example/config/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/css/**", "/js/**", "/fonts/**", "img/**").permitAll()
+                        .requestMatchers("/", "index.html", "registration.html").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/customers/register").permitAll()
+                        .anyRequest().authenticated()
+
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login.html")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
