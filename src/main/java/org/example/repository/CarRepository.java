ed88c926eb3f2d9b6dfc17a9538db1a0532336ba
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-04-25 15:07:52 +0200

    Add query to findAvailableCars

--- src/main/java/org/example/repository/CarRepository.java
+++ src/main/java/org/example/repository/CarRepository.java
@@ -1,15 +1,15 @@
 package org.example.repository;
 
-
 import org.example.model.Car;
-import org.example.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
- //   List<Car> findByAvailabilityDateBetween(Date startDatum, Date endDatum);
+    @Query("SELECT c FROM Car c WHERE c.id NOT IN (" +
+            "SELECT o.car.id FROM Order o WHERE o.pickUpDate <= :returnDate AND o.bringBackDate >= :pickupDate)")
+    List<Car> findAvailableCars(@Param("pickupDate") LocalDate pickupDate, @Param("returnDate") LocalDate returnDate);
 }
-
