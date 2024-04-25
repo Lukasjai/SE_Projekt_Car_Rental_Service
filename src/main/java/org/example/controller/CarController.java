37080cf9ce7ac3fe0833c22805ec56b0ed708857
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-04-25 15:10:04 +0200

    refactor findAvailableCars

--- src/main/java/org/example/controller/CarController.java
+++ src/main/java/org/example/controller/CarController.java
@@ -1,14 +1,12 @@
 package org.example.controller;
 
-import java.util.Date;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 import jakarta.servlet.http.HttpSession;
+import org.example.dto.CarAvailabilityDto;
 import org.example.model.Car;
-import org.example.repository.CarRepository;
 import org.example.service.CarService;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,27 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/cars")
 public class CarController {
 
-    private final CarRepository carRepository;
     private final CarService carService;
 
-    public CarController(CarRepository carRepository, CarService carService) {
-        this.carRepository = carRepository;
+    public CarController(CarService carService) {
         this.carService = carService;
     }
 
-    private static final String template = "Car available: %s";
-    private final AtomicLong counter = new AtomicLong();
+    @PostMapping("/availability")
+    public ResponseEntity<?> findAvailableCars(@RequestBody CarAvailabilityDto carAvailabilityDto) {
+        List<Car> availableCars = carService.findAvailableCars(carAvailabilityDto.getPickupDate(), carAvailabilityDto.getReturnDate());
 
-    @PostMapping("/verfuegbar")
-    public ResponseEntity<?> findAvailableCars(@RequestParam("pick_up_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate,
-                                                                         @RequestParam("bring_back_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate) {
-   //     List<org.example.model.Car> availableCars = carRepository.findByAvailabilityDateBetween(startDate, endDate);
-        return ResponseEntity.ok("");
-    }
-
-    public ResponseEntity<List<org.example.model.Car>> getAllCars() {
-        List<org.example.model.Car> cars = carRepository.findAll();
-        return ResponseEntity.ok(cars);
+        return ResponseEntity.status(HttpStatus.OK).body(availableCars);
     }
 
     @GetMapping("/carinfo")
@@ -44,30 +32,4 @@ public class CarController {
         Long carId = (Long) session.getAttribute("carId");
         return ResponseEntity.ok(carId);
     }
-
-/*    @GetMapping("/rent")
-    public Car rent(@RequestParam(value = "name", defaultValue = "Fiat") String name) {
-        return new Car(counter.incrementAndGet(), String.format(template, name));
-    }*/
-
-    }
-
-
-
-    /*
-    @PostMapping("/available")
-    public ResponseEntity<?> carsAvailable(@RequestBody DateWrapper dateWrapper) {
-        Optional<Car> carOptional = Optional.ofNullable(carRepository.findCarById(car));
-        if (carOptional.isPresent()) {
-            return ResponseEntity.ok(carOptional.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-
-
-        System.out.println("todo");
-        return ResponseEntity.notFound().build();
-    }
-*/
-
-
+}
