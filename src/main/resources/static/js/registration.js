395c15e702e34a136e866cea51dfa8237fd7c057
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-04-25 10:18:29 +0200

    Fix registration to correctly send POST

--- src/main/resources/static/js/registration.js
+++ src/main/resources/static/js/registration.js
@@ -1,15 +1,22 @@
-document.getElementById("registrationForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-    var form = event.target;
-    var formData = new FormData(form); // Create FormData object from form data
-    // Send POST request to registration endpoint
-    fetch(form.action, {
-        method: "POST",
-        body: formData
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const formData = {
+        firstName: document.getElementById('firstName').value,
+        lastName: document.getElementById('lastName').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+        phoneNumber: document.getElementById('phoneNumber').value,
+        licenceNumber: document.getElementById('licenceNumber').value
+    };
+    fetch('/customers/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
     })
         .then(response => {
             if (response.ok) {
-                //alert("Registration successful!");
                 window.location.href = "/login.html";
             } else {
                 alert("Registration failed. Please try again.");
