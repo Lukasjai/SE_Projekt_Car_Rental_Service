9c929e3bab1218cdc90902edc53f5abe7c8cb58d
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-04-25 15:10:34 +0200

    implement findAvailableCars

--- src/main/java/org/example/service/CarService.java
+++ src/main/java/org/example/service/CarService.java
@@ -1,37 +1,33 @@
 package org.example.service;
 
-
 import org.example.dto.CarDto;
 import org.example.model.Car;
 import org.example.repository.CarRepository;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
 @Service
 public class CarService {
 
+    private final CarRepository carRepository;
 
 
-
-        private final CarRepository carRepository;
-
-
-        public CarService(CarRepository carRepository) {
-            this.carRepository = carRepository;
-        }
-
-        public Car callCars(CarDto carDto) {
-            Car car = new Car();
-            car.setCar_brand_name(carDto.getCar_brand_name());
-            car.setCar_model_name(carDto.getCar_model_name());
-            car.setNumber_of_seats(carDto.getNumber_of_seats());
-            car.setPrices(carDto.getPrice());
-            return carRepository.save(car);
-        }
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
     }
 
+    public Car callCars(CarDto carDto) {
+        Car car = new Car();
+        car.setCar_brand_name(carDto.getCar_brand_name());
+        car.setCar_model_name(carDto.getCar_model_name());
+        car.setNumber_of_seats(carDto.getNumber_of_seats());
+        car.setPrices(carDto.getPrice());
+        return carRepository.save(car);
+    }
 
-
-
-
-
+    public List<Car> findAvailableCars(LocalDate pickupDate, LocalDate returnDate) {
+        return carRepository.findAvailableCars(pickupDate, returnDate);
+    }
+}
