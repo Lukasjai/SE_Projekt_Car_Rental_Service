4f5f76cf7e6b2048c1838a684c87a6a44698df85
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-04-27 11:15:23 +0200

    remove unused callCars method

--- src/main/java/org/example/service/CarService.java
+++ src/main/java/org/example/service/CarService.java
@@ -1,6 +1,5 @@
 package org.example.service;
 
-import org.example.dto.CarDto;
 import org.example.model.Car;
 import org.example.repository.CarRepository;
 import org.springframework.stereotype.Service;
@@ -13,18 +12,8 @@ public class CarService {
 
     private final CarRepository carRepository;
 
-
     public CarService(CarRepository carRepository) {
         this.carRepository = carRepository;
-    }
-
-    public Car callCars(CarDto carDto) {
-        Car car = new Car();
-        car.setCar_brand_name(carDto.getCar_brand_name());
-        car.setCar_model_name(carDto.getCar_model_name());
-        car.setNumber_of_seats(carDto.getNumber_of_seats());
-        car.setPrices(carDto.getPrice());
-        return carRepository.save(car);
     }
 
     public List<Car> findAvailableCars(LocalDate pickupDate, LocalDate returnDate) {
