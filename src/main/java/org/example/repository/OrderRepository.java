f1c714968b1af901a5ebc70cefc3187f09fe8918
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-04-27 19:57:01 +0200

    add findByCustomerMail

--- src/main/java/org/example/repository/OrderRepository.java
+++ src/main/java/org/example/repository/OrderRepository.java
@@ -3,4 +3,8 @@ package org.example.repository;
 import org.example.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByCustomerEmail(String email);
+}
