8f4fe7db7002b095f56f5a586101721ffbe5bb4a
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-04-27 08:52:35 +0200

    allow contact.html to be viewed without logging in

--- src/main/java/org/example/config/WebSecurityConfig.java
+++ src/main/java/org/example/config/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig {
                 .csrf().disable()
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/css/**", "/js/**", "/fonts/**", "img/**").permitAll()
-                        .requestMatchers("/", "index.html", "registration.html").permitAll()
+                        .requestMatchers("/", "index.html", "registration.html", "contact.html").permitAll()
                         .requestMatchers(HttpMethod.POST, "/customers/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/customers/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/customers/logout").permitAll()
