677ae0523a6613fd28dd3fbe776af99a6ffb8acc
Lukas Jaiczay <lukas.jaiczay@stud.fh-campuswien.ac.at>
2024-04-28 11:59:13 +0200

    ConvertCurreny calls the api of the Currency Converter

---
+++ src/main/java/org/example/currenyConverterConnection/ConvertCurrency.java
@@ -0,0 +1,111 @@
+
+package org.example.currenyConverterConnection;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java-Klasse für anonymous complex type.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * 
+ * <pre>{@code
+ * <complexType>
+ *   <complexContent>
+ *     <restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       <sequence>
+ *         <element name="amount" type="{http://www.w3.org/2001/XMLSchema}double"/>
+ *         <element name="fromCurrency" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         <element name="toCurrency" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       </sequence>
+ *     </restriction>
+ *   </complexContent>
+ * </complexType>
+ * }</pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "amount",
+    "fromCurrency",
+    "toCurrency"
+})
+@XmlRootElement(name = "ConvertCurrency")
+public class ConvertCurrency {
+
+    protected double amount;
+    @XmlElement(required = true, nillable = true)
+    protected String fromCurrency;
+    @XmlElement(required = true, nillable = true)
+    protected String toCurrency;
+
+    /**
+     * Ruft den Wert der amount-Eigenschaft ab.
+     * 
+     */
+    public double getAmount() {
+        return amount;
+    }
+
+    /**
+     * Legt den Wert der amount-Eigenschaft fest.
+     * 
+     */
+    public void setAmount(double value) {
+        this.amount = value;
+    }
+
+    /**
+     * Ruft den Wert der fromCurrency-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFromCurrency() {
+        return fromCurrency;
+    }
+
+    /**
+     * Legt den Wert der fromCurrency-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFromCurrency(String value) {
+        this.fromCurrency = value;
+    }
+
+    /**
+     * Ruft den Wert der toCurrency-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getToCurrency() {
+        return toCurrency;
+    }
+
+    /**
+     * Legt den Wert der toCurrency-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setToCurrency(String value) {
+        this.toCurrency = value;
+    }
+
+}
