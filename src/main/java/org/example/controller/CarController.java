82e57755919177990f97ee3dc67b0688a7d103f9
Lukas Jaiczay <lukas.jaiczay@stud.fh-campuswien.ac.at>
2024-04-29 23:41:19 +0200

    currencyConverter functionalitys implemented in CarController splitted in two Post functions

--- src/main/java/org/example/controller/CarController.java
+++ src/main/java/org/example/controller/CarController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.example.dto.CarAvailabilityDto;
 import org.example.model.Car;
 import org.example.service.CarService;
+import org.example.currencyConverter.CurrencyConverter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/v1/cars")
@@ -19,10 +21,24 @@ public class CarController {
         this.carService = carService;
     }
 
-    @PostMapping
+    @PostMapping("/findAvailable")
     public ResponseEntity<?> findAvailableCars(@RequestBody CarAvailabilityDto carAvailabilityDto) {
         List<Car> availableCars = carService.findAvailableCars(carAvailabilityDto.getPickupDate(), carAvailabilityDto.getReturnDate());
 
         return ResponseEntity.status(HttpStatus.OK).body(availableCars);
     }
+
+    @PostMapping("/updatePrice")
+    public ResponseEntity<?> updatePriceOfAvailableCars(@RequestBody CarAvailabilityDto carAvailabilityDto, @RequestParam String currency) {
+        List<Car> availableCars = carService.findAvailableCars(carAvailabilityDto.getPickupDate(), carAvailabilityDto.getReturnDate());
+        CurrencyConverter currencyConverter = new CurrencyConverter();
+        for (Car car : availableCars) {
+            System.out.println("update PRice");
+            double convertedPrice = currencyConverter.convertCurrency(car.getPrices(), "USD", currency);
+            car.setPrices((float) convertedPrice);
+
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(availableCars);
+    }
+
 }
