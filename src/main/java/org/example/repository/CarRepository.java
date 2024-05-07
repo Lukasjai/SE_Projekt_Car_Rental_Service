6f5f868d4c7be63127ac1affe5ecb02ed3da15c9
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-05-07 10:37:48 +0200

    add repository annotation

--- src/main/java/org/example/repository/CarRepository.java
+++ src/main/java/org/example/repository/CarRepository.java
@@ -4,10 +4,12 @@ import org.example.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
     @Query("SELECT c FROM Car c WHERE c.id NOT IN (" +
             "SELECT o.car.id FROM Order o WHERE o.pickUpDate <= :returnDate AND o.bringBackDate >= :pickupDate)")
--- src/main/java/org/example/repository/OrderRepository.java
+++ src/main/java/org/example/repository/OrderRepository.java
@@ -2,9 +2,11 @@ package org.example.repository;
 
 import org.example.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByCustomerEmail(String email);
 }
