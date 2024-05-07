3268453f454ff40f3fa5c94715c24f9c1c8a1a27
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2024-05-07 17:10:56 +0200

    refactor variable names to camelCase

--- src/main/java/org/example/model/Car.java
+++ src/main/java/org/example/model/Car.java
@@ -6,73 +6,69 @@ import jakarta.persistence.*;
 @Table(name = "car_inventory")
 public class Car {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "car_id")
+    private Long id;
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "car_id")
-        private Long id;
+    @Column(name = "car_brand_name", nullable = false, columnDefinition = "CHAR")
+    private String brandName;
 
-        @Column(name = "car_brand_name", nullable = false, columnDefinition = "CHAR")
-        private String car_brand_name;
-        @Column(name = "car_model_name", nullable = false, columnDefinition = "CHAR")
-        private String car_model_name;
+    @Column(name = "car_model_name", nullable = false, columnDefinition = "CHAR")
+    private String modelName;
 
-        @Column(name = "number_of_seats")
-        private int number_of_seats;
-        @Column(name = "prices", nullable = false)
-        private float prices;
+    @Column(name = "number_of_seats")
+    private int numberOfSeats;
 
+    @Column(name = "prices", nullable = false)
+    private float price;
 
-        public Car() {}
+    public Car() {}
 
-        public Car(String car_brand_name, String car_model_name, int number_of_seats, float prices) {
-            this.car_brand_name = car_brand_name;
-            this.car_model_name = car_model_name;
-            this.number_of_seats = number_of_seats;
-            this.prices = prices;
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getCar_brand_name() {
-            return car_brand_name;
-        }
-
-        public void setCar_brand_name(String car_brand_name) {
-            this.car_brand_name = car_brand_name;
-        }
-
-        public String getCar_model_name() {
-            return car_model_name;
-        }
-
-        public void setCar_model_name(String car_model_name) {this.car_model_name = car_model_name;
-        }
-
-        public int getNumber_of_seats() {
-            return number_of_seats;
-        }
-
-        public void setNumber_of_seats(int number_of_seats) {
-            this.number_of_seats = number_of_seats;
-        }
-
-        public float getPrices() {
-            return prices;
-        }
-
-        public void setPrices(float prices) {
-            this.prices = prices;
-        }
-
-
+    public Car(String brandName, String modelName, int numberOfSeats, float price) {
+        this.brandName = brandName;
+        this.modelName = modelName;
+        this.numberOfSeats = numberOfSeats;
+        this.price = price;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
