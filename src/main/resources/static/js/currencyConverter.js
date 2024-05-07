03e91a0f5b82c29ff466ba733f61262013c894a0
Sahani <sahani070797@gmail.com>
2024-05-07 09:39:17 +0200

    Price changed to Price/Day

--- src/main/resources/static/js/currencyConverter.js
+++ src/main/resources/static/js/currencyConverter.js
@@ -49,7 +49,7 @@ function displayCarsWithNewPrice(cars) {
         brandCell.innerHTML = `Brand: ${car.car_brand_name}`;
         modelCell.innerHTML = `Model: ${car.car_model_name}`;
         seatsCell.innerHTML = `Seats: ${car.number_of_seats}`;
-        priceCell.innerHTML = `Price: ${car.prices}${currencySymbol}`;
+        priceCell.innerHTML = `Price/Day: ${car.prices}${currencySymbol}`;
 
         const bookButtonCell = carRow.insertCell();
         const bookButton = document.createElement('button');
